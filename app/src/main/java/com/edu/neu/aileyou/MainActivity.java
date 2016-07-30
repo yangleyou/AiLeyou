618d7cc8b73ba7c7ced5344c537b1954bf2564f0
yangleyou <535932485@qq.com>
2016-07-30 12:01:07 +0800

    完成首页图片轮播效果

--- app/src/main/java/com/edu/neu/aileyou/MainActivity.java
+++ app/src/main/java/com/edu/neu/aileyou/MainActivity.java
@@ -1,13 +1,76 @@
 package com.edu.neu.aileyou;
 
+import android.content.Context;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.bigkoo.convenientbanner.ConvenientBanner;
+import com.bigkoo.convenientbanner.holder.CBViewHolderCreator;
+import com.bigkoo.convenientbanner.holder.Holder;
+import com.edu.neu.aileyou.util.LocalImageHolderView;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
+    ConvenientBanner convenientBanner;
+    List<Integer> localImages = new ArrayList<Integer>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initBanner();
+
+
+    }
+
+    private void initBanner() {
+        loadTestDatas();
+        convenientBanner=(ConvenientBanner)findViewById(R.id.convenientBanner);
+
+        //自定义你的Holder，实现更多复杂的界面，不一定是图片翻页，其他任何控件翻页亦可。
+        convenientBanner.setPages(
+                new CBViewHolderCreator<LocalImageHolderView>() {
+                    @Override
+                    public LocalImageHolderView createHolder() {
+                        return new LocalImageHolderView();
+                    }
+                }, localImages);
+        //设置两个点图片作为翻页指示器，不设置则没有指示器，可以根据自己需求自行配合自己的指示器,不需要圆点指示器可用不设
+        //.setPageIndicator(new int[]{R.drawable.ic_page_indicator, R.drawable.ic_page_indicator_focused})
+        //设置指示器的方向
+        //.setPageIndicatorAlign(ConvenientBanner.PageIndicatorAlign.ALIGN_PARENT_RIGHT)
+        //设置翻页的效果，不需要翻页效果可用不设
+        //.setPageTransformer(Transformer.DefaultTransformer);    集成特效之后会有白屏现象，新版已经分离，如果要集成特效的例子可以看Demo的点击响应。
+        //convenientBanner.setManualPageable(false);//设置不能手动影响
+
+    }
+    private void loadTestDatas() {
+        //本地图片集合
+        for (int position = 0; position < 2; position++)
+        {
+            localImages.add(getResId("ic_test_" + position, R.drawable.class));
+        }
+    }
+
+    /**
+     * 通过文件名获取资源id 例子：getResId("icon", R.drawable.class);
+     *
+     * @param variableName
+     * @param c
+     * @return
+     */
+    public static int getResId(String variableName, Class<?> c) {
+        try {
+            Field idField = c.getDeclaredField(variableName);
+            return idField.getInt(idField);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return -1;
+        }
     }
 }
---
+++ app/src/main/java/com/edu/neu/aileyou/util/LocalImageHolderView.java
@@ -0,0 +1,26 @@
+package com.edu.neu.aileyou.util;
+
+import android.content.Context;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.bigkoo.convenientbanner.holder.Holder;
+
+/**
+ * Created by Administrator on 2016/7/30.
+ */
+
+public class LocalImageHolderView implements Holder<Integer> {
+    private ImageView imageView;
+    @Override
+    public View createView(Context context) {
+        imageView = new ImageView(context);
+        imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+        return imageView;
+    }
+
+    @Override
+    public void UpdateUI(Context context, final int position, Integer data) {
+        imageView.setImageResource(data);
+    }
+}
